1d7afe7b28306877ae326b48e1f198e5d606f55d
PortableDoughnut <gwen.thelin@gmail.com>
2024-10-15 10:30:31 -0600

    Completed the first five steps of step 3

--- Meal Tracker/FoodTableViewController.swift
+++ Meal Tracker/FoodTableViewController.swift
@@ -8,22 +8,35 @@
 import UIKit
 
 class FoodTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	static var eggs: Food = Food(name: "eggs", description: "Sunny side up eggs straight from the farm.")
+	static var bacon: Food = Food(name: "bacon", description: "Crispy bacon cooked to perfection.")
+	static var toast: Food = Food(name: "toast", description: "Wheat toast cooked to a golden brown.")
+	
+	let breakfastArray: [Food] = [eggs, bacon, toast]
+	
+	static var cheeseburger: Food = Food(name: "cheeseburger", description: "A classic American burger with melted cheese.")
+	static var frenchFries: Food = Food(name: "frenchFries", description: "Crispy french fries cooked to perfection.")
+	let lunchArray: [Food] = [cheeseburger, frenchFries]
+	
+	static var pizza: Food = Food(name: "pizza", description: "A slice of pizza with your choice of toppings.")
+	static var breadSticks: Food = Food(name: "breadSticks", description: "Crispy bread sticks cooked to perfection.")
+	
+	let DinnerArray: [Food] = [pizza, breadSticks]
+	
+	var meals: [Meal] {
+		[Meal(name: "breakfast", food: breakfastArray), Meal(name: "lunch", food: lunchArray), Meal(name: "dinner", food: DinnerArray)]
+	}
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		<#code#>
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		<#code#>
+	}
 }
